a38e62c01d0f7ed9999b917d6ab0c2ff799fd8a8
lmpko <lmpkoo@gmail.com>
2023-05-11 12:39:09 +0200

    images set as background into logo container for better manipulation

--- src/FaqComponent.js
+++ src/FaqComponent.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react'
-import illustration from "/images/illustration-woman-online-mobile.svg"
-import bgPattern from "/images/bg-pattern-mobile.svg"
 import QAcomponent from './QAcomponent'
 import { qa } from '../config'
 
@@ -11,16 +9,18 @@ const FaqComponent = () => {
   <>
     <section className='faq-container'>
         <div className='logo-container'>
-            <img src={illustration} alt="illustration" />
-            <img src={bgPattern} alt="bg" />
+            
         </div>
-        <h1>FAQ</h1>
-        <div className='qa-container'>
-            {qa.map((qa,index) => <QAcomponent qa={qa} key={qa.id}
-              index={index}
-              activeIndex={activeIndex}
-              setActiveIndex={setActiveIndex} />) }
+        <div className='faq-big-container'>
+          <h1>FAQ</h1>
+          <div className='qa-container'>
+              {qa.map((qa,index) => <QAcomponent qa={qa} key={qa.id}
+                index={index}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex} />) }
+          </div>
         </div>
+        
     </section>
   </>
   )
